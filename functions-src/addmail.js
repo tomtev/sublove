8a8480bc1bdd8dbf28a4d35cab8dc8498f855257
Tommy Vedvik <tommyvedvik@gmail.com>
2018-04-13 12:24:26 +0200

    fix

--- functions-src/addmail.js
+++ functions-src/addmail.js
@@ -1,4 +1,3 @@
 exports.handler = function(event, context, callback) {
-  console.log(event.body)
   console.log(JSON.stringify(event.body))
 }
--- functions/addmail.js
+++ functions/addmail.js
@@ -71,7 +71,6 @@
 
 
 exports.handler = function (event, context, callback) {
-  console.log(event.body);
   console.log(JSON.stringify(event.body));
 };
 
