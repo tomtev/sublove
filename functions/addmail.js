e9cf05f1a1f2265b5c45a2137bd3fe23ab4850aa
Tommy Vedvik <tommyvedvik@gmail.com>
2018-04-13 10:20:48 +0200

    test

--- functions/addmail.js
+++ functions/addmail.js
@@ -1,5 +1,16 @@
 exports.handler = function(event, context, callback) {
-  console.log('**** EVENT ****', +event)
-  console.log('***** CONTEXT ******', +context)
-  console.log('***** CALLBACK ******' +callback)
+
+  //-- We only care to do anything if this is our POST request.
+  if(event.httpMethod !== 'POST' || !event.body) {
+    callback(null, {
+      statusCode,
+      headers,
+      body: ''
+    });
+  }
+
+  //-- Parse the body contents into an object.
+  const data = JSON.parse(event.body);
+
+  console.log(data)
 }
